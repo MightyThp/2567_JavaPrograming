bb8288df19aa7c83254400a32baada8aff7e7323
MightyThp <samakaguya551@gmail.com>
2024-12-18 11:16:53 +0700

    labsheet04practice

---
+++ Labsheet04/src/Lab404.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class Lab404 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		inputStudent();
+		
+	}
+
+	public static void inputStudent() {
+		Scanner scanner= new Scanner(System.in);
+		String studentID, subjectCode;
+		boolean isStudentIDValid, isSubjectCodeValid;
+		while(true) {
+			System.out.print("Enter student ID (10digits): ");
+			studentID= scanner.nextLine();
+			
+			System.out.print("Enter subject code (7 digits): ");
+			subjectCode =scanner.nextLine();
+			
+			isStudentIDValid = isLength(studentID,10);
+			isSubjectCodeValid = isLength(subjectCode,7);
+			
+			if (isStudentIDValid && isSubjectCodeValid ) {
+				
+				boolean isITStudent = isITStudent(studentID);
+				boolean isITSubject = isITSubject(subjectCode);
+				
+				displayData(isITStudent,isITSubject);
+				break;
+			}else {
+				System.out.println("");
+			}
+		}
+	}
+	public static boolean isLength(String input,int len) {
+		return input.length() == len;
+	}
+}
