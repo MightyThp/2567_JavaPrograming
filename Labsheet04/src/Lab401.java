6f997bd18466a0003ddb4bacc9492b57d780ebf9
MightyThp <samakaguya551@gmail.com>
2024-12-18 09:59:47 +0700

    Labsheet04Example

--- Labsheet04/src/Lab401.java
+++ Labsheet04/src/Lab401.java
@@ -1,9 +1,41 @@
-
+import javax.swing.*;
 public class Lab401 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		inputEmail();
 	}
-
+	
+	public static void inputEmail() {
+		String ans;
+		String varEmail;
+		do {
+			varEmail = JOptionPane.showInputDialog("Input your e-mail:");
+			if(varEmail!=null&&!varEmail.isEmpty()) {
+				checkEmailError(varEmail);
+			}else {
+				JOptionPane.showConfirmDialog(null, "E-mail cannot be empty!!!");
+			}
+			
+			ans=JOptionPane.showInputDialog("Do you want to input e-mail address[Y/y]:");
+		}while(ans!=null&&ans.equalsIgnoreCase("y"));
+	}
+	public static void checkEmailError(String email) {
+	
+		while(email.startsWith("@")||email.contains(" ")){
+			email=JOptionPane.showInputDialog("Invalid e-mail. Input tour e-mailagain:");
+		}
+		email=email.toLowerCase();
+		JOptionPane.showMessageDialog(null, 
+				checkEmail(email)
+				?"Your e-mail is "+email
+				:"Your e-mail is not a hotmail or gmail address."
+		);
+	}
+	public static boolean checkEmail(String email) {
+		return email.endsWith("@gmail.com")||email.endsWith("hotmail.com");
+		
+	}
+	
 }
+
--- Labsheet04/src/Lab403.java
+++ Labsheet04/src/Lab403.java
@@ -1,9 +1,36 @@
-
+import java.util.*;
 public class Lab403 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		String fullName;
+		String firstName;
+		
+		System.out.println("Please enter your name, seperated by a space.");
+		System.out.print(":");
+		fullName=input.nextLine();
+		
+		int space = fullName.indexOf(' ');
+		firstName = fullName.substring(0,space);
+		
+		System.out.println(abbreviatName(fullName)+firstName);
+		
+		
+		
+		input.close();
 	}
 
+	public static String abbreviatName(String fName) {
+		String initialLetter="";
+		
+		for (int i=0;i<fName.length();i++) {
+			if(fName.charAt(i)==' ') {
+				initialLetter =(initialLetter+fName.charAt(i+1)).toUpperCase();	
+				initialLetter = initialLetter+".";
+			}
+		}
+		return initialLetter;
+	}
+	
 }
